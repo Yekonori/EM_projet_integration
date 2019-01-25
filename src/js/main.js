e850993b73cbd4744fd7b9333433510ff97dd5f6
Yannis <yannisdemacedo@gmail.com>
2019-01-25 16:48:50 +0100

    Debug

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
+// On attend que la page soit totalement chargée :
 $(document).ready(function () {
     // NAVBAR MOBILE
     $(".cross").hide();
     $(".menu").hide();
+    // Ouverture du menu
     $(".hamburger").click(function () {
         $(".menu").slideToggle("slow", function () {
             $(".hamburger").hide();
             $(".cross").show();
         });
     });
-
+    // Fermeture du menu
     $(".cross").click(function () {
         $(".menu").slideToggle("slow", function () {
             $(".cross").hide();
@@ -18,13 +20,18 @@ $(document).ready(function () {
 
     // CAROUSEL
     $(".owl-carousel").owlCarousel({
+        // On autorise la boucle
         loop: true,
+        // On applique un margin a chaque card
         margin: 20,
+        // On retire la navigation par défaut
         nav: false,
+        // Et on crée nos propres boutons
         navText: [
             '<button class="btn-carousel slick slick-prev"><i class="fas fa-chevron-left"></i></button>',
             '<button class="btn-carousel slick slick-next"><i class="fas fa-chevron-right"></i></button>'
         ],
+        // Gestion du responsive
         responsive: {
             0: {
                 items: 1
